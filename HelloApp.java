4405ff02be3b1b55153258139a4e570537cfac01
vv3293-a11y <vv3293@srmist.edu.in>
2026-03-11 23:07:19 +0530

    Coded UC2: Display Hello with Command-Line Argument

--- HelloApp.java
+++ HelloApp.java
@@ -1,7 +1,16 @@
+
+
 public class HelloApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, vansh!");
+        }
+
     }
 
 }
