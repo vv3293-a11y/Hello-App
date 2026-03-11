6f87bdb74886deab9b2dbb4e29eb6e66c494b4f1
vv3293-a11y <vv3293@srmist.edu.in>
2026-03-11 23:21:41 +0530

    Coded UC1: Display Hello World in console

---
+++ HelloApp1.java
@@ -0,0 +1,15 @@
+/**
+ * Hello Application
+ * Displays Hello World on the console
+ * 
+ * @author Vansh
+ * @version 1.0
+ */
+
+public class HelloApp1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
